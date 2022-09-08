d30ce4f497a209cba03c991d1676ac8fe723af43
Liz Lawler <eslawler@gmail.com>
2022-09-07 22:05:20 -0600

    change parameter from xi to kappa everywhere

--- sim-study/models/g1/R/g1_NUTS_kappa-only.R
+++ sim-study/models/g1/R/g1_NUTS_kappa-only.R
@@ -36,28 +36,28 @@ saveRDS(egpd_fit, file = "./sim-study/models/g1/stan-fits/g1_kappa-only.RDS")
 
 
 ## ------
-post <- rstan::extract(egpd_fit, pars = c("beta_xi", "phi_xi"))
-median_xi <- apply(post$beta_xi, c(2,3), median)
+post <- rstan::extract(egpd_fit, pars = c("beta_kappa", "phi_kappa"))
+median_kappa <- apply(post$beta_kappa, c(2,3), median)
 
-post_xi_effects_df <- matrix(NA, r, t)
+post_kappa_effects_df <- matrix(NA, r, t)
 for(i in 1:r) {
-  post_xi_effects_df[i,] <- X_full[i, , ] %*% median_xi[, i]
+  post_kappa_effects_df[i,] <- X_full[i, , ] %*% median_kappa[, i]
 }
 
-post_xi <- t(post_xi_effects_df) %>% as_tibble() %>% 
+post_kappa <- t(post_kappa_effects_df) %>% as_tibble() %>% 
   rename_with(., ~ reg_cols) %>% 
   mutate(time = c(1:t)) %>%
   pivot_longer(cols = c(1:all_of(r)), values_to = "effect", names_to = "region") %>%
   left_join(., X_long) %>%
   left_join(., mod_reg_key) %>% mutate(type = "sim")
-xi_full <- rbind(xi_effects, post_xi) %>% mutate(type = factor(type, levels = c("truth", "sim")))
-post_effects_xi <- ggplot(xi_full, aes(x=linear, y=effect, group = region)) + 
+kappa_full <- rbind(kappa_effects, post_kappa) %>% mutate(type = factor(type, levels = c("truth", "sim")))
+post_effects_kappa <- ggplot(kappa_full, aes(x=linear, y=effect, group = region)) + 
   geom_line(aes(linetype=NA_L1CODE, color = NA_L2CODE)) + 
   facet_grid(type ~ .)
-ggsave(paste0('./sim-study/figures/g1/effects/g1_post-effects-xi_',
+ggsave(paste0('./sim-study/figures/g1/effects/g1_post-effects-kappa_',
               format(as.POSIXlt(Sys.time(), "America/Denver"), "%d%b%Y_%H%M"),
               ".pdf"),
-       plot=post_effects_xi,
+       plot=post_effects_kappa,
        device = "pdf")
 
 
@@ -93,6 +93,6 @@ full_phi_onetime_kappa <- ecoregions_geom %>%
 ggsave(paste0('./sim-study/figures/g1/maps/g1_phi-map-kappa_',
               format(as.POSIXlt(Sys.time(), "America/Denver"), "%d%b%Y_%H%M"),
               ".pdf"),
-       plot=full_phi_onetime_xi,
+       plot=full_phi_onetime_kappa,
        device = "pdf")
 

40a5b10c122e20536ab0a5178d0bfe0060e5e44e
Liz Lawler <eslawler@gmail.com>
2022-11-09 19:37:05 -0700

    update g1 shell scripts with new jobname structure

--- call_g1_NUTS_sampler.sh
+++ call_g1_NUTS_sampler.sh
@@ -3,4 +3,4 @@
 module load anaconda
 conda activate lawler
 
-Rscript full-model/fire-sims/burns/g1/R/g1_NUTS_sampling.R ${suffix:-NULL} ${params:-NULL} > full-model/output/g1_$(printf %s ${suffix:-NULL} ${params:-NULL}).txt 2>&1
+Rscript full-model/fire-sims/burns/g1/R/g1_NUTS_sampling.R ${suffix:-NULL} ${params:-NULL}

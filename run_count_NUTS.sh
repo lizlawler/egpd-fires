231ceeb0b0d09ecb8ea46682706ac1752842ad03
Liz Lawler <eslawler@gmail.com>
2022-11-09 19:12:04 -0700

    test 'output' file change

--- run_count_NUTS.sh
+++ run_count_NUTS.sh
@@ -13,6 +13,6 @@ for model in "zinb" "zinb_er" "zip"
 do
 for params in "pi-reg" "pi-ri"
 do
-sbatch --job-name count_NUTS_sampling --nodes=1 --ntasks-per-node=4 --time=24:00:00 --mail-type=ALL --mail-user=eslawler@colostate.edu --export=model=$model,params=$params call_count_NUTS_sampler.sh
+sbatch --job-name count_NUTS_sampling --output='./full-model/output/g1_$(printf %s $model _ $params).txt' --nodes=1 --ntasks-per-node=4 --time=24:00:00 --mail-type=ALL --mail-user=eslawler@colostate.edu --export=model=$model,params=$params call_count_NUTS_sampler.sh
 done
 done

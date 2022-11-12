9eba8eb019627c882155c3e8807198e8f596fa5e
Liz Lawler <eslawler@colostate.edu@c3cpu-a5-u21-2.rc.int.colorado.edu>
2022-11-12 16:05:35 -0700

    Increase requested cores from 10 to 14 in shell script

--- run_count_NUTS.sh
+++ run_count_NUTS.sh
@@ -13,6 +13,6 @@ for model in "zinb" "zinb_er" "zip"
 do
 for params in "pi-reg" "pi-ri"
 do
-sbatch --job-name count_$(printf %s $model "_" $params) --output='./full-model/output/%x_%j.txt' --nodes=1 --ntasks-per-node=10 --time=24:00:00 --mail-type=ALL --mail-user=eslawler@colostate.edu --export=model=$model,params=$params call_count_NUTS_sampler.sh
+sbatch --job-name count_$(printf %s $model "_" $params) --output='./full-model/output/%x_%j.txt' --nodes=1 --ntasks-per-node=14 --time=48:00:00 --mail-type=ALL --mail-user=eslawler@colostate.edu --export=model=$model,params=$params call_count_NUTS_sampler.sh
 done
 done

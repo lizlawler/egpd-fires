beb929e95b0f62b07b0e63d0255dab3fb72da26a
Liz Lawler <eslawler@gmail.com>
2022-11-12 16:25:45 -0700

    update file paths to reflect new shell-scripts directory

--- shell-scripts/run_g2_NUTS.sh
+++ shell-scripts/run_g2_NUTS.sh
@@ -13,6 +13,6 @@ for suffix in "sqrt" "og"
 do
 for params in "nu-reg_xi-reg" "nu-reg_xi-ri" "nu-ri_xi-ri"
 do
-sbatch --job-name g2_$(printf %s $params "_" $suffix) --output='./full-model/output/output_%x_%j.txt' --nodes=1 --ntasks-per-node=4 --time=24:00:00 --mail-type=ALL --mail-user=eslawler@colostate.edu --export=suffix=$suffix,params=$params call_g2_NUTS_sampler.sh
+sbatch --job-name g2_$(printf %s $suffix "_" $params) --chdir=/scratch/alpine/eslawler@colostate.edu/egpd-fires/ --output='./full-model/output/%x_%j.txt' --qos=long --nodes=1 --ntasks-per-node=14 --time=72:00:00 --mail-type=ALL --mail-user=eslawler@colostate.edu --export=suffix=$suffix,params=$params shell-scripts/call_g2_NUTS_sampler.sh
 done
 done

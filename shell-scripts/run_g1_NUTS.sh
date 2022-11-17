e2249c2e055ebd45e0d47650127a178d382262cf
Liz Lawler <eslawler@gmail.com>
2022-11-17 10:10:02 -0700

    decrease requested cores to 20 from 25

--- shell-scripts/run_g1_NUTS.sh
+++ shell-scripts/run_g1_NUTS.sh
@@ -13,6 +13,6 @@ for suffix in "sqrt" "og"
 do
 for params in "nu-reg_xi-reg" "nu-reg_xi-ri" "nu-ri_xi-ri"
 do
-sbatch --job-name g1_$(printf %s $suffix "_" $params) --chdir=/scratch/alpine/eslawler@colostate.edu/egpd-fires/ --output='./full-model/output/%x_%j.txt' --qos=long --nodes=1 --ntasks-per-node=25 --time=72:00:00 --mail-type=ALL --mail-user=eslawler@colostate.edu --export=suffix=$suffix,params=$params shell-scripts/call_g1_NUTS_sampler.sh
+sbatch --job-name g1_$(printf %s $suffix "_" $params) --chdir=/scratch/alpine/eslawler@colostate.edu/egpd-fires/ --output='./full-model/output/%x_%j.txt' --qos=long --nodes=1 --ntasks-per-node=20 --time=72:00:00 --mail-type=ALL --mail-user=eslawler@colostate.edu --export=suffix=$suffix,params=$params shell-scripts/call_g1_NUTS_sampler.sh
 done
 done

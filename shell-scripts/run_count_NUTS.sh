c2172093ad2bd205d3188e4803c6149d0b794ea0
Liz Lawler <eslawler@gmail.com>
2023-01-12 14:51:01 +0000

    increase allotted time and requested cores

--- shell-scripts/run_count_NUTS.sh
+++ shell-scripts/run_count_NUTS.sh
@@ -13,6 +13,6 @@ for model in "zinb" "zinb_er" "zip"
 do
 for params in "pi-reg" "pi-ri"
 do
-sbatch --job-name count_$(printf %s $model "_" $params) --chdir=/scratch/alpine/eslawler@colostate.edu/egpd-fires/ --output='./full-model/output/%x_%j.txt' --qos=long --nodes=1 --ntasks-per-node=20 --time=48:00:00 --mail-type=ALL --mail-user=eslawler@colostate.edu --export=model=$model,params=$params shell-scripts/call_count_NUTS_sampler.sh
+sbatch --job-name count_$(printf %s $model "_" $params) --chdir=/scratch/alpine/eslawler@colostate.edu/egpd-fires/ --output='./full-model/output/%x_%j.txt' --qos=long --nodes=1 --ntasks-per-node=20 --time=72:00:00 --mail-type=ALL --mail-user=eslawler@colostate.edu --export=model=$model,params=$params shell-scripts/call_count_NUTS_sampler.sh
 done
 done

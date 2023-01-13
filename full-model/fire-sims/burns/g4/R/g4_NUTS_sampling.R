13042cc3d7213ef41aa4f562c538f5e576b7e8b7
Liz Lawler <eslawler@gmail.com>
2023-01-13 17:00:00 +0000

    save full fit object instead of params

--- full-model/fire-sims/burns/g4/R/g4_NUTS_sampling.R
+++ full-model/fire-sims/burns/g4/R/g4_NUTS_sampling.R
@@ -24,7 +24,7 @@ options(mc.cores = parallel::detectCores())
 st_time <- format(as.POSIXlt(Sys.time(), "America/Denver"), "%d-%b-%Y_%H%M")
 
 stan_data <- readRDS(paste0("./full-model/data/stan_data_", suffix, ".rds"))
-egpd_init <- stan_model(paste0('./full-model/fire-sims/burns/g3/stan/g3_', params, '.stan'))
+egpd_init <- stan_model(paste0('./full-model/fire-sims/burns/g4/stan/g4_', params, '.stan'))
 egpd_fit <- sampling(egpd_init, 
                      data = stan_data, 
                      iter = 3000,
@@ -35,8 +35,8 @@ egpd_fit <- sampling(egpd_init,
 end_time <- format(as.POSIXlt(Sys.time(), "America/Denver"), "%H%M")
 
 # save MCMC object in case below dx plots don't save properly
-post <- rstan::extract(egpd_fit, pars = c("beta", "phi", "rho1", "rho2", "holdout_loglik", "train_loglik"))
-saveRDS(post, file = paste0("./full-model/fire-sims/burns/g3/stan-fits/", params,"_loglik_", suffix, "_", 
+# post <- rstan::extract(egpd_fit, pars = c("beta", "phi", "rho1", "rho2", "holdout_loglik", "train_loglik"))
+saveRDS(egpd_fit, file = paste0("./full-model/fire-sims/burns/g4/stan-fits/", params,"_loglik_", suffix, "_", 
                             st_time, "_", end_time, ".RDS"))
 
 # save traceplot
@@ -44,48 +44,48 @@ if(params == 'nu-reg_xi-reg') {
   MCMCtrace(egpd_fit, params = c("rho1_delta", "rho2_delta", "rho1_nu", "rho2_nu", "rho1_xi", "rho2_xi"),
             ind = TRUE,
             open_pdf = FALSE,
-            filename = paste0('./full-model/figures/g3/trace/g3_nu-reg_xi-reg_', suffix, '_rhos_',
+            filename = paste0('./full-model/figures/g4/trace/g4_nu-reg_xi-reg_', suffix, '_rhos_',
                               st_time, "_", end_time, ".pdf"))
   MCMCtrace(egpd_fit, params = c("beta_delta", "beta_nu", "beta_xi"),
             ind = TRUE,
             open_pdf = FALSE,
-            filename = paste0('./full-model/figures/g3/trace/g3_nu-reg_xi-reg_', suffix, '_betas_',
+            filename = paste0('./full-model/figures/g4/trace/g4_nu-reg_xi-reg_', suffix, '_betas_',
                               st_time, "_", end_time, ".pdf"))
   MCMCtrace(egpd_fit, params = c("phi_delta", "phi_nu", "phi_xi"),
             ind = TRUE,
             open_pdf = FALSE,
-            filename = paste0('./full-model/figures/g3/trace/g3_nu-reg_xi-reg_', suffix, '_phis_',
+            filename = paste0('./full-model/figures/g4/trace/g4_nu-reg_xi-reg_', suffix, '_phis_',
                               st_time, "_", end_time, ".pdf"))
 } else if (params == 'nu-reg_xi-ri') {
   MCMCtrace(egpd_fit, params = c("rho1_delta", "rho2_delta", "rho1_nu", "rho2_nu", "rho1_xi", "rho2_xi"),
             ind = TRUE,
             open_pdf = FALSE,
-            filename = paste0('./full-model/figures/g3/trace/g3_nu-reg_xi-ri_', suffix, '_rhos_',
+            filename = paste0('./full-model/figures/g4/trace/g4_nu-reg_xi-ri_', suffix, '_rhos_',
                               st_time, "_", end_time, ".pdf"))
   MCMCtrace(egpd_fit, params = c("beta_delta", "beta_nu"),
             ind = TRUE,
             open_pdf = FALSE,
-            filename = paste0('./full-model/figures/g3/trace/g3_nu-reg_xi-ri_', suffix, '_betas_',
+            filename = paste0('./full-model/figures/g4/trace/g4_nu-reg_xi-ri_', suffix, '_betas_',
                               st_time, "_", end_time, ".pdf"))
   MCMCtrace(egpd_fit, params = c("phi_delta", "phi_nu"),
             ind = TRUE,
             open_pdf = FALSE,
-            filename = paste0('./full-model/figures/g3/trace/g3_nu-reg_xi-ri_', suffix, '_phis_',
+            filename = paste0('./full-model/figures/g4/trace/g4_nu-reg_xi-ri_', suffix, '_phis_',
                               st_time, "_", end_time, ".pdf"))
 } else {
   MCMCtrace(egpd_fit, params = c("rho1_delta", "rho2_delta", "rho1_nu", "rho2_nu", "rho1_xi", "rho2_xi"),
             ind = TRUE,
             open_pdf = FALSE,
-            filename = paste0('./full-model/figures/g3/trace/g3_nu-ri_xi-ri_', suffix, '_rhos_',
+            filename = paste0('./full-model/figures/g4/trace/g4_nu-ri_xi-ri_', suffix, '_rhos_',
                               st_time, "_", end_time, ".pdf"))
   MCMCtrace(egpd_fit, params = c("beta_delta"),
             ind = TRUE,
             open_pdf = FALSE,
-            filename = paste0('./full-model/figures/g3/trace/g3_nu-ri_xi-ri_', suffix, '_betas_',
+            filename = paste0('./full-model/figures/g4/trace/g4_nu-ri_xi-ri_', suffix, '_betas_',
                               st_time, "_", end_time, ".pdf"))
   MCMCtrace(egpd_fit, params = c("phi_delta"),
             ind = TRUE,
             open_pdf = FALSE,
-            filename = paste0('./full-model/figures/g3/trace/g3_nu-ri_xi-ri_', suffix, '_phis_',
+            filename = paste0('./full-model/figures/g4/trace/g4_nu-ri_xi-ri_', suffix, '_phis_',
                               st_time, "_", end_time, ".pdf"))
 }

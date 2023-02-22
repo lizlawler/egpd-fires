d69384ccd662258f455e3680a955b8063a7065d6
Liz Lawler <eslawler@gmail.com>
2023-02-22 15:22:46 -0700

    added '--vanilla' back into shell script

--- shell-scripts/call_g1_NUTS_sampler.sh
+++ shell-scripts/call_g1_NUTS_sampler.sh
@@ -3,5 +3,5 @@
 source /curc/sw/anaconda3/2020.11/etc/profile.d/conda.sh
 conda activate /projects/eslawler@colostate.edu/software/anaconda/envs/lawler
 
-Rscript full-model/fire-sims/burns/g1/g1_NUTS_sampling.R \
+Rscript --vanilla full-model/fire-sims/burns/g1/g1_NUTS_sampling.R \
 ${suffix:-NULL} ${params:-NULL}

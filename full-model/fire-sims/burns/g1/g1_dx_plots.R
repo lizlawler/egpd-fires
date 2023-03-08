2777a4860963a454d5ea986f4ed7d6af06bc720a
Liz Lawler <eslawler@gmail.com>
2023-03-08 12:23:54 -0700

    update format for cmdstan_fit readin to "draws_list"

--- full-model/fire-sims/burns/g1/g1_dx_plots.R
+++ full-model/fire-sims/burns/g1/g1_dx_plots.R
@@ -23,7 +23,7 @@ csv_files <- paste0("./full-model/fire-sims/burns/g1/csv-fits/",
                                pattern = file_base))
 st_date <- stringr::str_extract(basename(csv_files[1]), "\\d{2}-Mar-\\d{4}")
 
-egpd_fit <- as_cmdstan_fit(csv_files)
+egpd_fit <- as_cmdstan_fit(csv_files, format = "draws_list")
 egpd_fit$save_obj(file = paste0("./full-model/fire-sims/burns/g1/cmd-stan-fits/g1_", suffix, "_", params, "_", st_date, ".RDS"))
 egpd_mcmc <- as_mcmc.list(egpd_fit)
 

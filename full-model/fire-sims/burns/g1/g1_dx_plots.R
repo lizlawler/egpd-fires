c7e550eaea5ce06c46ca16cb734c93b6e8bb9ecd
Liz Lawler <eslawler@gmail.com>
2023-03-13 12:32:28 -0600

    Update scripts to troubleshoot 'grep' errors

--- full-model/fire-sims/burns/g1/g1_dx_plots.R
+++ full-model/fire-sims/burns/g1/g1_dx_plots.R
@@ -9,13 +9,18 @@ if (!(args[2] %in% c("all-reg", "xi-ri", "sigma-ri_xi-ri", "nu-ri_xi-ri", "kappa
 suffix <- args[1]
 params <- args[2]
 
-# library(cmdstanr)
-# set_cmdstan_path(path = "/projects/eslawler@colostate.edu/.cmdstan/cmdstan-2.31.0") # this is only relevant to Alpine
-# check_cmdstan_toolchain(fix = TRUE, quiet = TRUE)
-library(brms)
-library(MCMCvis)
-library(stringr)
+library(cmdstanr)
+set_cmdstan_path(path = "/projects/eslawler@colostate.edu/.cmdstan/cmdstan-2.31.0") # this is only relevant to Alpine
+check_cmdstan_toolchain(fix = TRUE, quiet = TRUE)
+# library(brms)
+# library(MCMCvis)
+# library(stringr)
 options(mc.cores = parallel::detectCores())
+Sys.getenv(c("TMP","TMPDIR"))
+Sys.setenv("TMPDIR" = "/scratch/alpine/eslawler@colostate.edu/")
+Sys.setenv("TMP" = "/scratch/alpine/eslawler@colostate.edu/")
+Sys.getenv(c("TMP","TMPDIR"))
+
 
 file_base <- paste0("g1_", suffix, "_", params, "*")
 csv_files <- paste0("./full-model/fire-sims/burns/g1/csv-fits/", 
@@ -24,25 +29,33 @@ csv_files <- paste0("./full-model/fire-sims/burns/g1/csv-fits/",
 print(csv_files)
 st_date <- stringr::str_extract(basename(csv_files[1]), "\\d{2}-Mar-\\d{4}")
 
-egpd_fit <- brms:::read_csv_as_stanfit(csv_files)
-print("created object successfully")
-saveRDS(egpd_fit, file = paste0("./full-model/fire-sims/burns/g1/stan-fits/g1_", suffix, "_", params, "_", st_date, ".RDS"))
-print("stanfit object saved successfully")
+basename(csv_files[1])
+egpd_fit1 <- as_cmdstan_fit(csv_files[1])
+basename(csv_files[2])
+egpd_fit2 <- as_cmdstan_fit(csv_files[2])
+basename(csv_files[3])
+egpd_fit3 <- as_cmdstan_fit(csv_files[3])
+
+
+# egpd_fit <- brms:::read_csv_as_stanfit(csv_files)
+# print("created object successfully")
+# saveRDS(egpd_fit, file = paste0("./full-model/fire-sims/burns/g1/stan-fits/g1_", suffix, "_", params, "_", st_date, ".RDS"))
+# print("stanfit object saved successfully")
 
 # save traceplots
-MCMCtrace(egpd_fit, params = "rho",
-          ind = TRUE,
-          open_pdf = FALSE,
-          filename = paste0('./full-model/figures/g1/trace/g1_', params, '_',
-                            suffix, '_rhos_', st_date, ".pdf"))
-MCMCtrace(egpd_fit, params = "beta",
-          ind = TRUE,
-          open_pdf = FALSE,
-          filename = paste0('./full-model/figures/g1/trace/g1_', params, '_',
-                            suffix,'_betas_', st_date, ".pdf"))
-MCMCtrace(egpd_fit, params = "phi",
-          ind = TRUE,
-          open_pdf = FALSE,
-          filename = paste0('./full-model/figures/g1/trace/g1_', params, '_',
-                            suffix,'_phis_', st_date, ".pdf"))
+#MCMCtrace(egpd_fit, params = "rho",
+#          ind = TRUE,
+#          open_pdf = FALSE,
+#          filename = paste0('./full-model/figures/g1/trace/g1_', params, '_',
+#                            suffix, '_rhos_', st_date, ".pdf"))
+#MCMCtrace(egpd_fit, params = "beta",
+#          ind = TRUE,
+#          open_pdf = FALSE,
+#          filename = paste0('./full-model/figures/g1/trace/g1_', params, '_',
+#                           suffix,'_betas_', st_date, ".pdf"))
+#MCMCtrace(egpd_fit, params = "phi",
+#          ind = TRUE,
+#          open_pdf = FALSE,
+#          filename = paste0('./full-model/figures/g1/trace/g1_', params, '_',
+#                            suffix,'_phis_', st_date, ".pdf"))
 
--- shell-scripts/call_dx_plots.sh
+++ shell-scripts/call_dx_plots.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-source /curc/sw/anaconda3/2020.11/etc/profile.d/conda.sh
-conda activate /projects/eslawler@colostate.edu/software/anaconda/envs/lawler
+module load anaconda
+conda activate lawler
 
 Rscript --vanilla full-model/fire-sims/burns/g1/g1_dx_plots.R \
 ${suffix:-NULL} ${params:-NULL}

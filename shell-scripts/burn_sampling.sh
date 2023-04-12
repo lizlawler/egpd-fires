ab75fe2694ec478e42ddb960e20271b104f0a065
Liz Lawler <eslawler@gmail.com>
2023-04-12 13:04:43 -0600

    increase warmup iter to 1500, decrease sampling to 1500 with no thinning

--- shell-scripts/burn_sampling.sh
+++ shell-scripts/burn_sampling.sh
@@ -8,12 +8,11 @@ datafile="../../../data/stan_data_${suffix}.json"
 basedir="./full-model/fire-sims/${modtype}/${modname}/"
 diagexe="/projects/eslawler@colostate.edu/software/anaconda/envs/stan/bin/cmdstan/bin/diagnose"
 cd ${basedir}
-sttime=$(date +"%d%b%Y_%H%M")
 model="stan/${modname}_${params}"
 outbase="csv-fits/${modname}_${suffix}_${params}_${delta}_${sttime}"
 
 # run model with 3 chains
-./${model} sample num_chains=3 num_warmup=1000 num_samples=2000 thin=2 \
+./${model} sample num_chains=3 num_warmup=1500 num_samples=1500 \
                   adapt delta=${delta} \
                   data file=${datafile} \
                   init=0.01 \

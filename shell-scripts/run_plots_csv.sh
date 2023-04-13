3fc432070b303c45c40140c752c08e23fafcb94f
Liz Lawler <eslawler@gmail.com>
2023-04-13 11:18:57 -0600

    create plots for jobs that ran out of memory

--- shell-scripts/run_plots_csv.sh
+++ shell-scripts/run_plots_csv.sh
@@ -6,11 +6,7 @@
 source /curc/sw/anaconda3/2022.10/etc/profile.d/conda.sh
 conda activate stan
 
-for csv in "g1_og_all-reg_0.81_27Mar2023_1528" "g1_og_all-reg_0.9_27Mar2023_1528" "g1_og_kappa-ri_xi-ri_0.81_27Mar2023_1834" "g1_og_kappa-ri_xi-ri_0.9_27Mar2023_1834" \
-"g1_og_nu-ri_xi-ri_0.81_27Mar2023_1712" "g1_og_nu-ri_xi-ri_0.9_27Mar2023_1739" "g1_og_sigma-ri_xi-ri_0.81_27Mar2023_1848" "g1_og_sigma-ri_xi-ri_0.9_27Mar2023_1848" \
-"g1_og_xi-ri_0.81_27Mar2023_1557" "g1_og_xi-ri_0.9_27Mar2023_1613" "g1_sqrt_all-reg_0.81_27Mar2023_1525" "g1_sqrt_all-reg_0.9_27Mar2023_1523" "g1_sqrt_kappa-ri_xi-ri_0.81_27Mar2023_1834" \
-"g1_sqrt_kappa-ri_xi-ri_0.9_27Mar2023_1834" "g1_sqrt_nu-ri_xi-ri_0.81_27Mar2023_1701" "g1_sqrt_nu-ri_xi-ri_0.9_27Mar2023_1701" "g1_sqrt_sigma-ri_xi-ri_0.81_27Mar2023_1834" \
-"g1_sqrt_sigma-ri_xi-ri_0.9_27Mar2023_1834" "g1_sqrt_xi-ri_0.81_27Mar2023_1528" "g1_sqrt_xi-ri_0.9_27Mar2023_1528"  
+for csv in "g1_og_kappa-ri_xi-ri_0.81_12Apr2023_1322" "g1_og_sigma-ri_xi-ri_0.81_12Apr2023_1323" "g1_og_all-reg_0.81_12Apr2023_1319"
 do
 export csv
 sbatch --job-name ${csv}_plots --output="./full-model/output/%x_%j.txt" \

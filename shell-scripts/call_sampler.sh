cac1fbf58387cee059a8b16ceb4a1aec8a64eafd
Liz Lawler <eslawler@gmail.com>
2023-05-05 16:09:10 -0600

    decrease requested cores from 5 to 4

--- shell-scripts/call_sampler.sh
+++ shell-scripts/call_sampler.sh
@@ -5,7 +5,7 @@
 #SBATCH --chdir=/scratch/alpine/eslawler@colostate.edu/egpd-fires/
 #SBATCH --qos=long
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=5
+#SBATCH --ntasks-per-node=4
 #SBATCH --time=120:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=eslawler@colostate.edu

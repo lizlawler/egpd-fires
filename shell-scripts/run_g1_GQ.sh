2808358dcc02ecf5292669e156814d70847e0d68
Liz Lawler <eslawler@gmail.com>
2023-06-01 23:11:55 -0600

    run 'run_g1_GQ' all 5 models instead of only 'sigma-ri' model

--- shell-scripts/run_g1_GQ.sh
+++ shell-scripts/run_g1_GQ.sh
@@ -10,23 +10,18 @@
 stanc_exe="/data/accounts/lawler/.conda/envs/stan/bin/cmdstan/bin/stanc"
 modtype="burns"
 modname="g1"
-# for params in "all-reg" "xi-ri" "nu-ri_xi-ri" "kappa-ri_xi-ri" "sigma-ri_xi-ri"
-# for params in "all-reg_cfcns" "xi-ri_cfcns" "nu-ri_xi-ri_cfcns" "kappa-ri_xi-ri_cfcns" "sigma-ri_xi-ri_cfcns"
-for params in "sigma-ri_xi-ri_cfcns"
+for params in "all-reg_cfcns" "xi-ri_cfcns" "nu-ri_xi-ri_cfcns" "kappa-ri_xi-ri_cfcns" "sigma-ri_xi-ri_cfcns"
 do
 # compile model and link c++ 
 inc_path="full-model/fire-sims/${modtype}/${modname}/stan/"
 object="full-model/fire-sims/${modtype}/${modname}/stan/${modname}_${params}"
 ${stanc_exe} ${object}.stan --include-paths=${inc_path}
 cmdstan_model ${object}
-# for suffix in "sqrt" "og"
-for suffix in "sqrt"
+for suffix in "sqrt" "og"
 do
-# for delta in 0.81 0.9
-for delta in 0.9
+for delta in 0.81 0.9
 do
-sttime="27Apr2023_1114"
-export modtype modname params suffix delta sttime \
+export modtype modname params suffix delta \
 # sbatch --job-name genquant_${modname}_${suffix}_${params}_${delta}_${sttime} \
 # --output="./full-model/output/%x_%j.txt" \
 shell-scripts/call_gq.sh

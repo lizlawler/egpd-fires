e5f974058ba1ed3820ca69b8f638fd0788ca3db9
Liz Lawler <eslawler@gmail.com>
2023-06-01 23:11:31 -0600

    loop through 3 chains of each model to create 3 csvs of gen quants for each model

--- shell-scripts/burn_gq.sh
+++ shell-scripts/burn_gq.sh
@@ -7,15 +7,14 @@
 datafile="../../../data/stan_data_${suffix}.json"
 basedir="./full-model/fire-sims/${modtype}/${modname}/"
 cd ${basedir}
-pwd
 model="stan/${modname}_${params}"
-fitted_files=csv-fits/${modname}_${suffix}_${params}_${delta}
+# fit=csv-fits/${modname}_${suffix}_${params}_${delta}
 
 for fit in csv-fits/${modname}_${suffix}_${params}_${delta}*.csv
 do
+  gq_file="$(basename -- $fit .csv)"
 # generate quantities using already fitted parameters
   ./${model} generate_quantities fitted_params=$fit \
                     data file=${datafile} \
-                    output file=${fit}_gq.csv &
-  echo "Generated quantities for $fit have been calculated"
+                    output file=csv-fits/${gq_file}_gq.csv &
 done

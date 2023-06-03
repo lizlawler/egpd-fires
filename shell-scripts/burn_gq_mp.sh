b13be8cd68f7cdab4ec625df5f080a34840a7555
Liz Lawler <eslawler@gmail.com>
2023-06-03 17:44:48 -0600

    add 'nohup' and 'trap "" HUP' to scripts and child processes for miss piggy runs

--- shell-scripts/burn_gq_mp.sh
+++ shell-scripts/burn_gq_mp.sh
@@ -1,20 +1,17 @@
 #!/bin/zsh
 # model run 
 
-# source /curc/sw/anaconda3/2022.10/etc/profile.d/conda.sh
-# conda activate stan
-
+trap '' HUP
 datafile="../../../data/stan_data_${suffix}.json"
 basedir="./full-model/fire-sims/${modtype}/${modname}/"
 cd ${basedir}
 model="stan/${modname}_${params}"
-# fit=csv-fits/${modname}_${suffix}_${params}_${delta}
 
 for fit in csv-fits/${modname}_${suffix}_${params}_${delta}*.csv
 do
   gq_file="$(basename -- $fit .csv)"
 # generate quantities using already fitted parameters
-  ./${model} generate_quantities fitted_params=$fit \
-                    data file=${datafile} \
-                    output file=csv-fits/${gq_file}_gq.csv &
+  nohup ./${model} generate_quantities fitted_params=$fit \
+                   data file=${datafile} \
+                   output file=csv-fits/${gq_file}_gq.csv &
 done
--- shell-scripts/run_g1_gq_mp.sh
+++ shell-scripts/run_g1_gq_mp.sh
@@ -2,11 +2,7 @@
 # shell script to call sbatch
 #
 # cycle through loop and launch sbatch for every combination
-#
-# source /curc/sw/anaconda3/2022.10/etc/profile.d/conda.sh
-# conda activate stan
-
-# stanc_exe="/projects/$USER/software/anaconda/envs/stan/bin/cmdstan/bin/stanc"
+trap '' HUP
 stanc_exe="/data/accounts/lawler/.conda/envs/stan/bin/cmdstan/bin/stanc"
 modtype="burns"
 modname="g1"
@@ -17,14 +13,12 @@ inc_path="full-model/fire-sims/${modtype}/${modname}/stan/"
 object="full-model/fire-sims/${modtype}/${modname}/stan/${modname}_${params}"
 ${stanc_exe} ${object}.stan --include-paths=${inc_path}
 cmdstan_model ${object}
-for suffix in "og"
+for suffix in "sqrt"
 do
-for delta in 0.9
+for delta in 0.81 0.9
 do
-export modtype modname params suffix delta \
-# sbatch --job-name genquant_${modname}_${suffix}_${params}_${delta}_${sttime} \
-# --output="./full-model/output/%x_%j.txt" \
-shell-scripts/call_gq.sh &
+export modtype modname params suffix delta
+nohup ./shell-scripts/burn_gq_mp.sh > full-model/output/${modname}_${suffix}_${params}_${delta}_gq.txt 2>&1 &
 sleep 1
 done
 done

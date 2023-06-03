3623f90a20c80ae9d859d616c84a23d558c5d4a2
Liz Lawler <eslawler@gmail.com>
2023-06-03 15:22:06 -0600

    modify 'burn_sampling' to run parallel chains via the shell instead of using 'num_chains' natively

--- shell-scripts/burn_sampling.sh
+++ shell-scripts/burn_sampling.sh
@@ -12,13 +12,14 @@ model="stan/${modname}_${params}"
 outbase="csv-fits/${modname}_${suffix}_${params}_${delta}_${sttime}"
 
 # run model with 3 chains
-./${model} sample num_chains=3 num_warmup=2000 num_samples=2000 thin=2 \
-                  adapt delta=${delta} \
-                  data file=${datafile} \
-                  init=0.01 \
-                  output file=${outbase}.csv \
-                  num_threads=3
-
+for i in {1..3} 
+  do
+    ./${model} sample num_warmup=2000 num_samples=2000 thin=2 \
+                      adapt delta=${delta} \
+                      data file=${datafile} \
+                      init=0.01 \
+                      output file=${outbase}_${i}.csv &
+  done
 # # return diagnostics
 # ${diagexe} ${outbase}_*.csv
 echo "Model has finished running all 3 chains"

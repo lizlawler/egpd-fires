7eab42ce87623958bb4a929be66fab35cb302464
Liz Lawler <eslawler@gmail.com>
2023-06-03 19:08:21 -0600

    export other possibly temp directories to my scratch directory on alpine

--- shell-scripts/call_sampler.sh
+++ shell-scripts/call_sampler.sh
@@ -6,12 +6,14 @@
 #SBATCH --qos=long
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=4
-#SBATCH --time=167:00:00
+#SBATCH --time=72:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=eslawler@colostate.edu
 
 export TMPDIR=/scratch/alpine/$USER/tmp/
 export TMP=${TMPDIR}
+export TEMP=${TMPDIR}
+export TEMPDIR=${TMPDIR}
 mkdir -p $TMPDIR
 
 source /curc/sw/anaconda3/2022.10/etc/profile.d/conda.sh

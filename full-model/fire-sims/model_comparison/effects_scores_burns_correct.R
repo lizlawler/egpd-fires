5dd089c27c0d646113e4c0da1f33e4630826ef5d
Liz Lawler <eslawler@gmail.com>
2023-06-13 12:03:00 -0600

    changes to processing raw climate data and calculation of FWI

--- full-model/fire-sims/model_comparison/effects_scores_burns_correct.R
+++ full-model/fire-sims/model_comparison/effects_scores_burns_correct.R
@@ -61,162 +61,161 @@ for(i in 1:20) {
   extraction(fit_groups[[i]])
 }
 
-# log score calculations ---------
-nfits <- length(burn_names)
-holdout_loglik_burns <- vector("list", nfits)
-train_loglik_burns <- vector("list", nfits)
-for(i in seq_along(burn_names)) {
-  # burn_loglik <- get(burn_names[i])[1:2]
-  holdout_loglik_burns[[i]] <- get(burn_names[i])[[2]]$generated_quantities %>%
-    apply(., c(1,2), sum) %>%
-    as_tibble() %>%
-    rowid_to_column(var = "iter") %>%
-    mutate(mean_ax_cns = rowMeans(select(., !iter))) %>%
-    pivot_longer(cols = !iter, names_to = "chain") %>%
-    mutate(chain = as.factor(chain)) %>%
-    mutate(model = burn_names[i], train = FALSE)
-  train_loglik_burns[[i]] <- get(burn_names[i])[[1]]$generated_quantities %>%
-    apply(., c(1,2), sum) %>%
-    as_tibble() %>%
-    rowid_to_column(var = "iter") %>%
-    mutate(mean_ax_cns = rowMeans(select(., !iter))) %>%
-    pivot_longer(cols = !iter, names_to = "chain") %>%
-    mutate(chain = as.factor(chain)) %>%
-    mutate(model = burn_names[i], train = TRUE)
-}
-
-holdout_loglik_b <- bind_rows(holdout_loglik_burns)
-train_loglik_b <- bind_rows(train_loglik_burns)
+burn_names <- lapply(fit_groups, function(x) str_remove(basename(x[1]), "_\\d{2}\\w{3}2023_\\d{4}_\\d{1}.csv")) %>% unlist()
+rm("g1_sqrt_all-reg_0.81")
+gc()
+save.image()
+## log score calculations ---------
+# holdout_loglik_counts <- vector("list", nfits)
+# train_loglik_counts <- vector("list", nfits)
+# for(i in seq_along(burn_names)) {
+#   count_loglik <- get(burn_names[i])[2:3]
+#   holdout_loglik_counts[[i]] <- count_loglik[[2]] %>%
+#     apply(., c(1,2), sum) %>% 
+#     as_tibble() %>% 
+#     rowid_to_column(var = "iter") %>%
+#     mutate(mean_ax_cns = rowMeans(select(., !iter))) %>%
+#     pivot_longer(cols = !iter, names_to = "chain") %>% 
+#     mutate(chain = as.factor(chain)) %>%
+#     mutate(model = burn_names[i], train = FALSE)
+#   train_loglik_counts[[i]] <- count_loglik[[1]] %>%
+#     apply(., c(1,2), sum) %>% 
+#     as_tibble() %>% 
+#     rowid_to_column(var = "iter") %>%
+#     mutate(mean_ax_cns = rowMeans(select(., !iter))) %>%
+#     pivot_longer(cols = !iter, names_to = "chain") %>% 
+#     mutate(chain = as.factor(chain)) %>%
+#     mutate(model = burn_names[i], train = TRUE)
+# }
+# 
+# holdout_loglik_c <- bind_rows(holdout_loglik_counts)
+# train_loglik_c <- bind_rows(train_loglik_counts)
 # # 
-ll_full <- holdout_loglik_b %>%
-  full_join(train_loglik_b) %>%
-  mutate(train = ifelse(train == TRUE, 'train', 'test')) %>%
-  pivot_wider(names_from = train, values_from = value)
-
-ll_full_0.81 <- ll_full %>% filter(grepl("0.81", model)) %>%
-  mutate(model = gsub("_0.81", "", model))
-ll_full_0.90 <- ll_full %>% filter(!grepl("0.81", model)) %>%
-  mutate(model = gsub("_0.9", "", model))
+# ll_full <- holdout_loglik_c %>%
+#   full_join(train_loglik_c) %>%
+#   mutate(train = ifelse(train == TRUE, 'train', 'test')) %>%
+#   pivot_wider(names_from = train, values_from = value)
 # 
-ll_boxplot_0.81_df <- ll_full_0.81 %>% pivot_longer(cols = c("test", "train"), names_to = "dataset") %>%
-  filter(chain == 'mean_ax_cns')
-
-
-std_0.81_df <- ll_full_0.81 %>% pivot_longer(cols = c("test", "train"), names_to = "dataset") %>% 
-  mutate(total_obs = case_when(dataset == "test" ~ 3314,
-                               dataset == "train" ~ 8018),
-         std_value = value/total_obs)
-  
-ll_box_0.81_test <- std_0.81_df %>% filter(chain == "mean_ax_cns", dataset == "test") %>% ggplot(aes(model, std_value, color = dataset)) + geom_boxplot() + theme_minimal()
-ll_box_0.81_train <- std_0.81_df %>% filter(chain == "mean_ax_cns", dataset == "train") %>% ggplot(aes(model, std_value, color = dataset)) + geom_boxplot() + theme_minimal()
-ll_box_0.81 <- std_0.81_df %>% filter(chain == "mean_ax_cns") %>% ggplot(aes(model, std_value, color = dataset)) + geom_boxplot() + theme_minimal()
-
-
-std_0.9_df <- ll_full_0.90 %>% pivot_longer(cols = c("test", "train"), names_to = "dataset") %>% 
-  mutate(total_obs = case_when(dataset == "test" ~ 3314,
-                               dataset == "train" ~ 8018),
-         std_value = value/total_obs)
-ll_box_0.9_test <- std_0.9_df %>% filter(chain == "mean_ax_cns", dataset == "test") %>% ggplot(aes(model, std_value, color = dataset)) + geom_boxplot() + theme_minimal()
-ll_box_0.9_train <- std_0.9_df %>% filter(chain == "mean_ax_cns", dataset == "train") %>% ggplot(aes(model, std_value, color = dataset)) + geom_boxplot() + theme_minimal()
-ll_box_0.9 <- std_0.9_df %>% filter(chain == "mean_ax_cns") %>% ggplot(aes(model, std_value, color = dataset)) + geom_boxplot() + theme_minimal()
-
-ll_ranked_test_0.81 <- ll_full_0.81 %>% filter(chain == 'mean_ax_cns') %>%
-  group_by(model) %>%
-  summarize(mean_test = mean(test),
-            sd_test = sd(test)) %>%
-  arrange(-mean_test)
-top_mod_test_0.81 <- ll_ranked_test_0.81$model[1]
+# ll_full_0.81 <- ll_full %>% filter(grepl("0.81", model)) %>% 
+#   mutate(model = gsub("_0.81", "", model),
+#          model = gsub("_og", "", model))
+# ll_full_0.90 <- ll_full %>% filter(!grepl("0.81", model)) %>% 
+#   mutate(model = gsub("_0.9", "", model),
+#          model = gsub("_og", "", model))
 # 
-ll_comp_test_0.81 <- ll_full_0.81 %>% filter(chain == 'mean_ax_cns') %>%
-  select(c(1, 3:4)) %>% pivot_wider(names_from = model, values_from = test) %>%
-  mutate(across(.cols = -1, ~ .x - get(top_mod_test_0.81))) %>%
-  pivot_longer(cols = -1, names_to = "model") %>%
-  group_by(model) %>%
-  summarize(mean_diff = mean(value),
-            sd_diff = sd(value)) %>%
-  arrange(-mean_diff)
-ll_comp_test_0.81
-
-ll_ranked_test_0.9 <- ll_full_0.90 %>% filter(chain == 'mean_ax_cns') %>%
-  group_by(model) %>%
-  summarize(mean_test = mean(test),
-            sd_test = sd(test)) %>%
-  arrange(-mean_test)
-top_mod_test_0.90 <- ll_ranked_test_0.9$model[1]
+# ll_boxplot_0.81 <- ll_full_0.81 %>% pivot_longer(cols = c("test", "train"), names_to = "dataset") %>% 
+#   filter(chain == 'mean_ax_cns') %>% 
+#   ggplot(aes(model, value, color = dataset)) + geom_boxplot() + theme_minimal()
+# ggsave("full-model/figures/model-comp/logscores_counts_0.81_12may2023.png", plot = ll_boxplot_0.81,
+#        dpi = 320, type = "cairo", bg = "white")
 # 
-ll_comp_test_0.9 <- ll_full_0.90 %>% filter(chain == 'mean_ax_cns') %>%
-  select(c(1, 3:4)) %>% pivot_wider(names_from = model, values_from = test) %>%
-  mutate(across(.cols = -1, ~ .x - get(top_mod_test_0.90))) %>%
-  pivot_longer(cols = -1, names_to = "model") %>%
-  group_by(model) %>%
-  summarize(mean_diff = mean(value),
-            sd_diff = sd(value)) %>%
-  arrange(-mean_diff)
-ll_comp_test_0.9
-ll_comp_test_0.81
-
-
-ll_ranked_train_0.81 <- ll_full_0.81 %>% 
-  select(-test) %>% 
-  filter(chain != "mean_ax_cns", train != Inf) %>% 
-  group_by(model) %>% 
-  summarize(mean_train = mean(train), sd_train = sd(train)) %>% 
-  arrange(-mean_train)
-top_mod_train_0.81 <- ll_ranked_train_0.81$model[1]
+# ll_boxplot_0.90 <- ll_full_0.90 %>% pivot_longer(cols = c("test", "train"), names_to = "dataset") %>% 
+#   filter(chain == 'mean_ax_cns') %>% 
+#   ggplot(aes(model, value, color = dataset)) + geom_boxplot() + theme_minimal()
+# ggsave("full-model/figures/model-comp/logscores_counts_0.90_12may2023.png", plot = ll_boxplot_0.90,
+#        dpi = 320, type = "cairo", bg = "white")
 # 
-ll_comp_train_0.81 <- ll_full_0.81 %>% 
-  select(-test) %>%
-  filter(chain != "mean_ax_cns", train != Inf) %>% 
-  group_by(model, iter) %>% 
-  summarize(mean = mean(train)) %>% 
-  pivot_wider(names_from = model, values_from = mean) %>%
-  mutate(across(.cols = -1, ~ .x - get(top_mod_train_0.81))) %>%
-  pivot_longer(cols = -1, names_to = "model") %>%
-  group_by(model) %>%
-  summarize(mean_diff = mean(value),
-            sd_diff = sd(value)) %>%
-  arrange(-mean_diff)
-ll_comp_train_0.81
-
-ll_ranked_train_0.90 <- ll_full_0.90 %>% 
-  select(-test) %>% 
-  filter(chain != "mean_ax_cns", train != Inf) %>% 
-  group_by(model) %>% 
-  summarize(mean_train = mean(train), sd_train = sd(train)) %>% 
-  arrange(-mean_train)
-top_mod_train_0.90 <- ll_ranked_train_0.90$model[1]
+# ll_ranked_test <- ll_full %>% filter(chain == 'mean_ax_cns') %>%
+#   group_by(model) %>%
+#   summarize(mean_test = mean(test),
+#             sd_test = sd(test)) %>%
+#   arrange(-mean_test)
+# top_mod_test <- ll_ranked_test$model[1]
 # 
-ll_comp_train_0.90 <- ll_full_0.90 %>% 
-  select(-test) %>%
-  filter(chain != "mean_ax_cns", train != Inf) %>% 
-  group_by(model, iter) %>% 
-  summarize(mean = mean(train)) %>% 
-  pivot_wider(names_from = model, values_from = mean) %>%
-  mutate(across(.cols = -1, ~ .x - get(top_mod_train_0.90))) %>%
-  pivot_longer(cols = -1, names_to = "model") %>%
-  group_by(model) %>%
-  summarize(mean_diff = mean(value),
-            sd_diff = sd(value)) %>%
-  arrange(-mean_diff)
-ll_comp_train_0.90
-
-# twcrps calculations ---------
-holdout_twcrps_burns <- vector("list", nfits)
-for(i in seq_along(burn_names)) {
-  # burn_loglik <- get(burn_names[i])[1:2]
-  holdout_twcrps_burns[[i]] <- get(burn_names[i])[[3]]$generated_quantities %>%
-    apply(., c(1,2), sum) %>%
-    as_tibble() %>%
-    rowid_to_column(var = "iter") %>%
-    mutate(mean_ax_cns = rowMeans(select(., !iter))) %>%
-    pivot_longer(cols = !iter, names_to = "chain") %>%
-    mutate(chain = as.factor(chain)) %>%
-    mutate(model = burn_names[i], train = FALSE)
-}
-
-holdout_twcrps_b <- bind_rows(holdout_twcrps_burns)
-holdout_twcrps_b
-
+# ll_comp_test <- ll_full %>% filter(chain == 'mean_ax_cns') %>% 
+#   select(c(1, 3:4)) %>% pivot_wider(names_from = model, values_from = test) %>% 
+#   mutate(across(.cols = -1, ~ .x - get(top_mod_test))) %>% 
+#   pivot_longer(cols = -1, names_to = "model") %>%
+#   group_by(model) %>%
+#   summarize(mean_diff = mean(value),
+#             sd_diff = sd(value)) %>%
+#   arrange(-mean_diff)
+# ll_comp_test
+# 
+# ll_ranked_test_0.81 <- ll_full_0.81 %>% filter(chain == 'mean_ax_cns') %>%
+#   group_by(model) %>%
+#   summarize(mean_test = mean(test),
+#             sd_test = sd(test)) %>%
+#   arrange(-mean_test)
+# top_mod_test_0.81 <- ll_ranked_test_0.81$model[1]
+# 
+# ll_comp_test_0.81 <- ll_full_0.81 %>% filter(chain == 'mean_ax_cns') %>% 
+#   select(c(1, 3:4)) %>% pivot_wider(names_from = model, values_from = test) %>% 
+#   mutate(across(.cols = -1, ~ .x - get(top_mod_test_0.81))) %>% 
+#   pivot_longer(cols = -1, names_to = "model") %>%
+#   group_by(model) %>%
+#   summarize(mean_diff = mean(value),
+#             sd_diff = sd(value)) %>%
+#   arrange(-mean_diff)
+# ll_comp_test_0.81
+# 
+# ll_ranked_test_0.90 <- ll_full_0.90 %>% filter(chain == 'mean_ax_cns') %>%
+#   group_by(model) %>%
+#   summarize(mean_test = mean(test),
+#             sd_test = sd(test)) %>%
+#   arrange(-mean_test)
+# top_mod_test_0.90 <- ll_ranked_test_0.90$model[1]
+# 
+# ll_comp_test_0.90 <- ll_full_0.90 %>% filter(chain == 'mean_ax_cns') %>% 
+#   select(c(1, 3:4)) %>% pivot_wider(names_from = model, values_from = test) %>% 
+#   mutate(across(.cols = -1, ~ .x - get(top_mod_test_0.90))) %>% 
+#   pivot_longer(cols = -1, names_to = "model") %>%
+#   group_by(model) %>%
+#   summarize(mean_diff = mean(value),
+#             sd_diff = sd(value)) %>%
+#   arrange(-mean_diff)
+# ll_comp_test_0.90
+# 
+# ll_ranked_train <- ll_full %>% filter(chain == 'mean_ax_cns') %>%
+#   group_by(model) %>%
+#   summarize(mean_train = mean(train),
+#             sd_train = sd(train)) %>%
+#   arrange(-mean_train)
+# top_mod_train <- ll_ranked_train$model[1]
+# 
+# ll_comp_train <- ll_full %>% filter(chain == 'mean_ax_cns') %>% 
+#   select(c(1,3,5)) %>% pivot_wider(names_from = model, values_from = train) %>% 
+#   mutate(across(.cols = -1, ~ .x - get(top_mod_train))) %>% 
+#   pivot_longer(cols = -1, names_to = "model") %>%
+#   group_by(model) %>%
+#   summarize(mean_diff = mean(value),
+#             sd_diff = sd(value)) %>%
+#   arrange(-mean_diff)
+# ll_comp_train
+# 
+# ll_ranked_train_0.81 <- ll_full_0.81 %>% filter(chain == 'mean_ax_cns') %>%
+#   group_by(model) %>%
+#   summarize(mean_train = mean(train),
+#             sd_train = sd(train)) %>%
+#   arrange(-mean_train)
+# top_mod_train_0.81 <- ll_ranked_train_0.81$model[1]
+# 
+# ll_comp_train_0.81 <- ll_full_0.81 %>% filter(chain == 'mean_ax_cns') %>% 
+#   select(c(1,3,5)) %>% pivot_wider(names_from = model, values_from = train) %>% 
+#   mutate(across(.cols = -1, ~ .x - get(top_mod_train_0.81))) %>% 
+#   pivot_longer(cols = -1, names_to = "model") %>%
+#   group_by(model) %>%
+#   summarize(mean_diff = mean(value),
+#             sd_diff = sd(value)) %>%
+#   arrange(-mean_diff)
+# ll_comp_train_0.81
+# 
+# ll_ranked_train_0.90 <- ll_full_0.90 %>% filter(chain == 'mean_ax_cns') %>%
+#   group_by(model) %>%
+#   summarize(mean_train = mean(train),
+#             sd_train = sd(train)) %>%
+#   arrange(-mean_train)
+# top_mod_train_0.90 <- ll_ranked_train_0.90$model[1]
+# 
+# ll_comp_train_0.90 <- ll_full_0.90 %>% filter(chain == 'mean_ax_cns') %>% 
+#   select(c(1,3,5)) %>% pivot_wider(names_from = model, values_from = train) %>% 
+#   mutate(across(.cols = -1, ~ .x - get(top_mod_train_0.90))) %>% 
+#   pivot_longer(cols = -1, names_to = "model") %>%
+#   group_by(model) %>%
+#   summarize(mean_diff = mean(value),
+#             sd_diff = sd(value)) %>%
+#   arrange(-mean_diff)
+# ll_comp_train_0.90
 # 
 # saveRDS(ll_full, file = "full-model/figures/model-comp/ll_full_counts_12may2023.RDS")
 ## ---------
@@ -266,36 +265,6 @@ covar_effect <- function(egpd_param_df, covar_term, linear_term) {
       mutate(region = as.numeric(region), covar = covar_term, linear = linear_term)
   )
 }
-
-temp_df <- `g1_sqrt_xi-ri_0.81`$betas %>%
-  mutate(dataset = 
-           case_when(grepl("g1_og", model_name) ~ "og",
-                     grepl("g1_sqrt", model_name) ~ "sqrt"),
-         combo = 
-           case_when(grepl("kappa-ri", model_name) ~ "kappa-ri",
-                     grepl("sigma-ri", model_name) ~ "sigma-ri",
-                     grepl("nu-ri", model_name) ~ "nu-ri",
-                     grepl("xi-ri", model_name) ~ "xi-ri",
-                     grepl("all-reg", model_name) ~ "all-reg",
-                     TRUE ~ model_name),
-         delta = 
-           case_when(grepl("0.81", model_name) ~ "0.81",
-                     grepl("0.9", model_name) ~ "0.9"),
-         model_name = paste0(dataset, "_", combo, "_", delta),
-         param = as.numeric(param),
-         coef = as.numeric(coef),
-         region = as.numeric(region))
-
-beta_vars <- variables(`g1_sqrt_xi-ri_0.81`[[1]]) %>% as_tibble() %>% separate(value, into = c("param", "covar", "region"), sep = ",") %>%
-  mutate(param = as.numeric(str_remove(param, "beta\\[")),
-         region = as.numeric(str_remove(region, "\\]")),
-         covar = as.numeric(covar))
-nu_beta <- beta_vars %>% filter(param == 2) %>% select(-param) %>% arrange(covar, region) %>% pivot_wider(names_from = region, values_from = value) %>%
-  select(-covar) %>% as.matrix()
-
-beta_vars <- `g1_sqrt_xi-ri_0.81`$betas %>% apply(., c(1,3), median) %>% as_tibble() %>% pivot_longer(everything(), names_to = "combo", values_to = "value")
-
-beta_vars <- beta_vars %>% separate(combo, into = c("param", "covar", "region"), sep = ",")
 
 for(i in seq_along(burn_beta_df_list)) {
   temp_df <- burn_beta_df_list[[i]] %>%

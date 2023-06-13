3255af20231d2e9da5c1c2cd6ba137d4a1966a11
Liz Lawler <eslawler@gmail.com>
2023-06-13 12:01:03 -0600

    changes to processing of raw climate data and calculation of FWI

--- full-model/data/process-data/aggregate-climate-data.R
+++ full-model/data/process-data/aggregate-climate-data.R
@@ -1,3 +1,4 @@
+# library(cffdrs)
 library(lubridate)
 library(tidyverse)
 library(parallel)
@@ -121,16 +122,89 @@ tifs_in_home_dir %>%
 
 print('Daily climate data aggregated to monthly summaries.')
 
-# library(cffdrs)
-# pr1983 <- rast("https://www.northwestknowledge.net/metdata/data/pr_1983.nc")
-# tmmx1983 <- rast("https://www.northwestknowledge.net/metdata/data/tmmx_1983.nc")
-# rmin1983 <- rast("https://www.northwestknowledge.net/metdata/data/rmin_1983.nc")
-# vs1983 <- rast("https://www.northwestknowledge.net/metdata/data/vs_1983.nc")
-# set.names(pr1983, rep("pr", 365))
-# set.names(tmmx1983, rep("temp", 365))
-# set.names(rmin1983, rep("rh", 365))
-# set.names(vs1983, rep("vs", 365))
-# 
-# full1983 <- c(tmmx1983, rmin1983, vs1983, pr1983)
-# res_init <- terra::tapp(full1983, 1:365, fun = fwiRaster)
+# need to store daily weather variables for use in FWI function ----------
+daily_over_usa <- function(url) {
+  # takes a climate data url and a masking spatial polygon as input
+  # and makes a returns daily variable, masked by the polygon
+  # pull the raw climate data file down locally
+  usa_shp <- terra::vect("full-model/data/raw/cb_2021_us_nation_20m",
+                         layer = "cb_2021_us_nation_20m")
+  
+  file <- basename(url)
+  
+  # generate an output filename
+  nc_name <- gsub(file,
+                  pattern = basename(file),
+                  replacement = paste0("daily_",
+                                       basename(file)))
+  out_name <- gsub(x = nc_name, pattern = ".nc", replacement = ".tif")
+  
+  # determine which climate variable, and which year we have
+  file_split <- unlist(strsplit(basename(url), split = "_"))
+  var <- file_split[1]
+  year <- substr(file_split[2], start = 1, stop = 4)
+  
+  # pass over file if necessary
+  if (out_name %in% list.files(pattern = basename(out_name), recursive = TRUE)) {
+    return(paste("File", out_name, "already exists"))
+  }
+  if (as.numeric(year) < 1983) {
+    return("Year outside range of consideration")
+  }
+  
+  # if we haven't already exited, download the original data file
+  download.file(url = url, destfile = file, timeout = 300)
+  
+  # generate monthly summary
+  raster <- terra::rast(file)
+  start_date <- as.Date(paste(year, "01", "01", sep = "-"))
+  end_date <- as.Date(paste(year, "12", "31", sep = "-"))
+  date_seq <- seq(start_date, end_date, by = "1 day")
+  date_seq <- date_seq[1:terra::nlyr(raster)]
+  month_seq <- lubridate::month(date_seq)
+  
+  names(raster) <- paste(var,
+                      unique(lubridate::date(date_seq)),
+                      sep = "_")
+  new_crs <- terra::crs(raster)
+  mask_shp <- terra::project(usa_shp, new_crs)
+  masked_raster <- terra::mask(raster, mask_shp)
+  terra::writeRaster(masked_raster, out_name, filetype = "GTiff")
+  unlink(file)
+  print(paste0("File ", out_name, " written"))
+}
 
+climate_data_urls <- read.csv('full-model/data/raw/climate-data.csv',
+                              stringsAsFactors = FALSE)
+print('Pulling daily weather data and masking by USA shapefile. May take a while...')
+pboptions(type = 'timer', use_lb = TRUE)
+cl <- makeCluster(getOption("cl.cores", detectCores()), outfile="")
+# may need to run this twice, to get timeout value correct 
+clusterEvalQ(cl, c(library("terra"), "daily_", options(timeout = 500))) 
+pblapply(X = climate_data_urls$url,
+         FUN = daily_over_usa,
+         cl = cl)
+stopCluster(cl)
+
+
+
+# Move files to proper directories -------------------------------------------
+tifs_in_home_dir <- list.files(pattern = ".tif") %>%
+  tibble(filename = .) %>%
+  separate(filename, into = c("time_interval", "variable", "year"), sep = "_") %>%
+  mutate(current_name = paste(time_interval, variable, year, sep = "_"))
+
+variables <- distinct(tifs_in_home_dir, variable) %>%
+  unlist()
+
+# create dirs for each variable
+dirs_to_make <- file.path("full-model/data", "processed", "climate-data", "daily", variables)
+sapply(dirs_to_make, dir.create, recursive = TRUE)
+
+# move each tif file to the proper location
+tifs_in_home_dir %>%
+  mutate(dest_dir = file.path("full-model/data", "processed", 'climate-data', "daily", variable),
+         dest_file = file.path(dest_dir, current_name)) %>%
+  do(file.rename(.$current_name, .$dest_file) %>% tibble)
+
+print('Daily climate data masked to USA polygons.')
--- full-model/data/process-data/get-ecoregion-summaries.R
+++ full-model/data/process-data/get-ecoregion-summaries.R
@@ -1,49 +1,53 @@
 library(tidyverse)
-# library(parallel)
-# library(pbapply)
+library(parallel)
+library(pbapply)
 library(terra)
 library(assertthat)
 source('./full-model/data/process-data/helpers.R')
 
 # Extracting monthly climate summaries for ecoregions ---------------------
-ecoregion_shp <- load_ecoregions() 
-
+ecoregion_shp <- load_ecoregions()
 # %>%
 #   project("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0")
 
-
 ecoregion_shp$NA_L3NAME <- as.character(ecoregion_shp$NA_L3NAME)
-
 ecoregion_shp$NA_L3NAME <- ifelse(ecoregion_shp$NA_L3NAME == 'Chihuahuan Desert',
                                   'Chihuahuan Deserts',
                                   ecoregion_shp$NA_L3NAME)
 
-tifs <- list.files("./full-model/data/processed/climate-data",
+# tifs <- list.files("./full-model/data/processed/climate-data",
+#                    pattern = ".tif",
+#                    recursive = TRUE,
+#                    full.names = TRUE)
+
+daily_tifs <- list.files("./full-model/data/processed/climate-data/daily",
                    pattern = ".tif",
                    recursive = TRUE,
                    full.names = TRUE)
 
 # remove any housing density geotiffs that matched the file listing
-tifs <- tifs[!grepl('den[0-9]{2}\\.tif', tifs)]
-
-# grab ERC tifs only
-erc_tifs <- tifs[grepl('erc_', tifs)]
+# tifs <- tifs[!grepl('den[0-9]{2}\\.tif', tifs)]
+# 
+# # grab ERC tifs only
+# erc_tifs <- tifs[grepl('erc_', tifs)]
 
 # Generate indices from polygons for raster extraction --------------------
-r <- raster::brick(tifs[1])
-shp_raster_idx <- cellFromPolygon(r, ecoregion_shp)
-names(shp_raster_idx) <- ecoregion_shp$NA_L3NAME
+r <- terra::rast(daily_tifs[1])
+tif_crs <- terra::crs(r)
+ecoregion_shp <- terra::project(ecoregion_shp, tif_crs)
+eco_names <- ecoregion_shp$NA_L3NAME %>% as_tibble() %>% rowid_to_column(var = "id") 
+shp_raster_idx_df <- terra::cells(r, ecoregion_shp) %>% as_tibble() %>%
+  rename(id = ID) %>% left_join(eco_names)
 
-# this list of indices has one element per polygon, but we want one per region
+# the above tibble of cell indices has one cell per polygon, but we want one per region (ie no duplicates)
 ecoregion_raster_idx <- vector(mode = 'list',
                                length = length(unique(ecoregion_shp$NA_L3NAME)))
 ecoregion_names <- sort(unique(ecoregion_shp$NA_L3NAME))
 names(ecoregion_raster_idx) <- ecoregion_names
 for (i in seq_along(ecoregion_names)) {
-  list_elements <- names(shp_raster_idx) == ecoregion_names[i]
-  assert_that(any(list_elements))
-  ecoregion_raster_idx[[i]] <- shp_raster_idx[list_elements] %>%
-    unlist
+  df_idx <- shp_raster_idx_df$value == ecoregion_names[i]
+  assert_that(any(df_idx))
+  ecoregion_raster_idx[[i]] <- unique(shp_raster_idx_df[df_idx, 2])
 }
 
 # verify that no cells are duplicated
@@ -57,24 +61,65 @@ assert_that(ecoregion_raster_idx %>%
               min > 0)
 
 # define an efficient extraction function to get mean values by polygon
-fast_extract <- function(rasterfile, index_list) {
-  r <- raster::brick(rasterfile)
-  var_yr <- stringr::str_replace(stringr::str_extract(rasterfile, "\\w+.\\d{4}"), "monthly_", "")
+# fast_extract_monthly <- function(rasterfile, index_list) {
+#   r <- terra::rast(rasterfile)
+# 
+#   polygon_means <- lapply(index_list, function(x) {
+#     extracts <- terra::extract(r, x$cell)
+#     colMeans(extracts, na.rm = TRUE)})
+# 
+#   list_of_dfs <- lapply(polygon_means, function(x) {
+#     df <- as.data.frame(x)
+#     colnames(df) <- "mean_by_er"
+#     df <- tibble::rownames_to_column(df) %>% 
+#       dplyr::rename(var_ymd = rowname)})
+# 
+#   merged_dfs <- dplyr::bind_rows(list_of_dfs, .id = 'NA_L3NAME')
+#   wide_df <- tidyr::pivot_wider(merged_dfs, names_from = var_ymd, values_from = mean_by_er)
+#   return(wide_df)
+# }
+# the below function creates csvs of the extracted raster values by ecoregion and by variable and year
+# takes about 2.5 hours on Macbook pro with M1 chip, 32 GB memory
+fast_extract_daily <- function(rasterfile, index_list) {
+  r <- terra::rast(rasterfile)
+  temp_name <- gsub(x = rasterfile,
+                  pattern = basename(rasterfile),
+                  replacement = paste0("by_er_", basename(rasterfile)))
+  out_name <- gsub(x = temp_name, pattern = ".tif", replacement = ".csv")
+  
+  if (out_name %in% list.files(pattern = basename(out_name), recursive = TRUE)) {
+    return(paste("File", out_name, "already exists"))
+  }
 
-  polygon_means <- lapply(index_list, function(x) {
-    extracts <- raster::extract(r, x)
-    colMeans(extracts, na.rm = TRUE)})
-
-  list_of_dfs <- lapply(polygon_means, function(x) {
-    df <- as.data.frame(x)
-    df <- tibble::rownames_to_column(df)
-    df <- dplyr::mutate(df, rowname = gsub("layer.", paste0(var_yr, "_"), rowname, fixed = TRUE))
-    dplyr::rename(df, var_ym = rowname)})
-
+  extracts <- lapply(index_list, function(x) terra::extract(r, x$cell))
+  
+  list_of_dfs <- lapply(extracts, function(x) {
+    df <- tibble::as_tibble(x)
+    df <- tibble::rowid_to_column(df) %>% 
+      dplyr::rename(id = rowid) %>%
+      tidyr::pivot_longer(!id, names_to = "var_ymd", values_to = "value") %>%
+      dplyr::filter(!is.na(value))})
+  
   merged_dfs <- dplyr::bind_rows(list_of_dfs, .id = 'NA_L3NAME')
-  wide_df <- tidyr::pivot_wider(merged_dfs, names_from = var_ym, values_from = x)
-  return(tibble::as_tibble(wide_df))
+  wide_df <- tidyr::pivot_wider(merged_dfs, names_from = var_ymd, values_from = value)
+  write_csv(wide_df, file = out_name)
+  print(paste0("File ", basename(out_name), " written"))
+  gc()
 }
+
+print('Aggregating daily climate data to ecoregion means. May take a while...')
+pboptions(type = 'timer', use_lb = TRUE)
+cl <- makeCluster(getOption("cl.cores", detectCores() / 2), outfile="")
+clusterEvalQ(cl, c(library("terra"), library("tidyverse"), "ecoregion_raster_idx", "fast_extract_daily"))
+extractions <- pblapply(daily_tifs, 
+                        fast_extract_daily, 
+                        index_list = ecoregion_raster_idx, 
+                        cl = cl)
+stopCluster(cl)
+
+# now create ecoregion specific csvs
+longer_daily <- by_er_daily_pr_2015 %>% pivot_longer(cols = !c("NA_L3NAME", "id"), names_to = "combo", values_to = "value")
+daily_rast_er <- longer_daily %>% mutate(rast_er = paste0())
 
 # Extract climate data ---------------------------------------
 print('Aggregating monthly climate data to ecoregion means. May take a while...')
@@ -88,24 +133,42 @@ stopCluster(cl)
 
 
 # Process extracted values into a usable data frame -----------------------
-ecoregion_summaries <- lapply(extractions, function(x) pivot_longer(x, !NA_L3NAME, names_to = "variable", values_to = "value")) %>% 
+daily_climate_eco <- lapply(extractions, function(x) pivot_longer(x, !NA_L3NAME, names_to = "var_ymd", values_to = "value")) %>% 
   bind_rows() %>%
-  separate(variable, into = c("variable", "year", "month"), sep = "_") %>%
+  separate(var_ymd, into = c("variable", "date"), sep = "_") %>%
+  separate(date, into = c("year", "month", "day"), sep = "-") %>%
   mutate(year = parse_number(year),
-         month = parse_number(month)) %>%
-  arrange(year, month, variable, NA_L3NAME)
+         month = parse_number(month),
+         day = parse_number(day)) %>%
+  arrange(year, month, day, variable, NA_L3NAME) %>% 
+  pivot_wider(., names_from = variable, values_from = value)
+eco_geom <- geom(ecoregion_shp) %>% as_tibble() %>% rename(id = geom) %>% left_join(eco_names) %>% dplyr::select(-c("id", "part", "hole"))
+eco_lat_long <- eco_geom %>% group_by(value) %>% summarise(long = mean(x), lat = mean(y)) %>% rename(NA_L3NAME = value)
+daily_climate_list <- daily_climate_eco %>% rename(yr = year, mon = month, temp = tmmx, rh = rmin, prec = pr, ws = vs) %>%
+  left_join(eco_lat_long) %>% split(., .$NA_L3NAME)
+daily_fwi_eco <- lapply(daily_climate_list, function(x) fwi(x, uppercase = FALSE, batch = TRUE, lat.adjust = TRUE))
 
 destfile <- "./full-model/data/processed/ecoregion_summaries.csv"
 write_csv(ecoregion_summaries, destfile)
 
 print(paste('Ecoregion climate summaries written to', destfile))
 
-# Calculate FWI from monthly weather variables
-library(cffdrs)
-ecoregion_summaries <- read_csv("data/processed/ecoregion_summaries.csv")
-ecoregion_summaries <- ecoregion_summaries %>% 
-  pivot_wider(names_from = variable, values_from = value) %>% 
-  rename(prec = pr, rh = rmin, temp = tmmx, ws = vs, mon = month, yr = year)
-summary_byer <- split(ecoregion_summaries, ecoregion_summaries$NA_L3NAME)
-fwi_list <- lapply(summary_byer, function(x) fwi(select(x, -NA_L3NAME)))
+# Process extracted values into a usable data frame -----------------------
+ecoregion_summaries <- extractions %>%
+  bind_cols %>%
+  gather(variable, value, -NA_L3NAME) %>%
+  filter(!grepl(pattern = 'NA_L3NAME', x = variable)) %>%
+  separate(variable,
+           into = c("interval", "variable", "timestep"),
+           sep = "_") %>%
+  separate(timestep, into = c("year", "month"), sep = "\\.") %>%
+  dplyr::select(-interval) %>%
+  mutate(year = parse_number(year),
+         month = parse_number(month)) %>%
+  arrange(year, month, variable, NA_L3NAME)
+
+destfile <- "data/processed/ecoregion_summaries.csv"
+write_csv(ecoregion_summaries, destfile)
+
+print(paste('Ecoregion climate summaries written to', destfile))
 

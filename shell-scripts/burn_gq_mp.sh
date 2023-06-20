2a16fb26909261b474e917b19ee8fa4690d688a8
Liz Lawler <eslawler@gmail.com>
2023-06-19 18:54:18 -0600

    remove 'cfcns' from file names in gen-quant call (lognorm csvs don't have this in name)

--- shell-scripts/burn_gq_mp.sh
+++ shell-scripts/burn_gq_mp.sh
@@ -8,7 +8,7 @@ cd ${basedir}
 model="stan/${modname}_${params}"
 sttime=$(date +"%d%b%Y_%H%M")
 
-for fit in csv-fits/${modname}_${suffix}_${params}_cfcns_${delta}*.csv
+for fit in csv-fits/${modname}_${suffix}_${params}_${delta}*.csv
 do
   gq_file="$(basename -- $fit .csv)"
 # generate quantities using already fitted parameters

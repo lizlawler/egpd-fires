1ef0bdaa862aeeef37faeaff0b812ce625dae96c
Liz Lawler <eslawler@gmail.com>
2023-07-31 11:54:01 -0600

    fix error in calling 'count_name'
    changed from 'count_name' to 'count_names'

--- full-model/fire-sims/model_comparison/extract_betas_best_counts.R
+++ full-model/fire-sims/model_comparison/extract_betas_best_counts.R
@@ -16,7 +16,7 @@ for(i in 1:nfits) {
   fit_groups[[i]] <- count_fits[(3*i-2):(3*i)]
 }
 
-count_name <- lapply(fit_groups, function(x) str_remove(basename(x[1]), "_\\d{2}\\w{3}2023_\\d{4}_\\d{1}.csv")) %>% unlist()
+count_names <- lapply(fit_groups, function(x) str_remove(basename(x[1]), "_\\d{2}\\w{3}2023_\\d{4}_\\d{1}.csv")) %>% unlist()
 
 extraction <- function(file_group, count_name) {
   object <- as_cmdstan_fit(file_group)

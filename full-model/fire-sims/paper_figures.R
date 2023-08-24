4f8aa7479962188debfd4103a4827c80af2b6576
Liz Lawler <eslawler@gmail.com>
2023-08-24 14:18:41 -0600

    Create maps of gamma and theta values to demonstrate linking behavior

--- full-model/fire-sims/paper_figures.R
+++ full-model/fire-sims/paper_figures.R
@@ -5,6 +5,8 @@ library(stringr)
 library(posterior)
 library(lubridate)
 library(sf)
+library(classInt)
+library(RColorBrewer)
 
 # following code is for extracting from the actual model fit -------
 burn_fits <- paste0("full-model/fire-sims/joint/sigma-ri/csv-fits/",
@@ -40,14 +42,24 @@ extraction <- function(file_group, burn_name) {
 
 extraction(best_group[[1]], "sigma_ri_theta-ri_gamma-ri")
 
+# egpd functions
 pegpd <- function(y, kappa, sigma, xi) (1 - (1 + xi * (y/sigma))^(-1/xi))^kappa
 qegpd <- function(p, kappa, sigma, xi) (sigma/xi) * ( (1 - p^(1/kappa) )^-xi - 1)
+# expected counts from ZINB params
 exp_count <- function(pi, lambda) { # expected counts
   pi_prob <- exp(pi)/(1+exp(pi))
   return((1-pi_prob) * exp(lambda))
 }
+
+# exceedance probabilities
 rlevel <- function(N, kappa, sigma, xi, eta) {
   p <- ((N-1)/N)^(1/(12 * eta)) * (1-pegpd(1.001, kappa, sigma, xi)) + pegpd(1.001, kappa, sigma, xi)
+  return(qegpd(p, kappa, sigma, xi))
+}
+
+# high quantiles
+high_quant <- rlevel <- function(N, kappa, sigma, xi) {
+  p <- ((N-1)/N) * (1-pegpd(1.001, kappa, sigma, xi)) + pegpd(1.001, kappa, sigma, xi)
   return(qegpd(p, kappa, sigma, xi))
 }
 
@@ -59,6 +71,7 @@ kappa_vals <- `sigma_ri_theta-ri_gamma-ri`$reg %>% as_draws_df() %>%
   mutate(time = as.numeric(gsub("reg\\[", "", time)),
          region = as.numeric(gsub("\\]", "", region)),
          kappa = exp(value)) %>% select(-value)
+# saveRDS(kappa_vals, file = "full-model/figures/paper/mcmc_draws/kappa_vals.RDS")
   
 rand_int <- `sigma_ri_theta-ri_gamma-ri`$ri_init %>% as_draws_df() %>%
   select(-c(".iteration", ".chain")) %>% 
@@ -84,6 +97,19 @@ lambda <- `sigma_ri_theta-ri_gamma-ri`$lambda %>% as_draws_df() %>%
          param = "lambda") %>%
   pivot_wider(names_from = param, values_from = value)
 
+phi <- `sigma_ri_theta-ri_gamma-ri`$phi %>% as_draws_df() %>%
+  select(-c(".iteration", ".chain")) %>% 
+  pivot_longer(cols = !".draw") %>%
+  rename(draw = ".draw") %>% 
+  separate_wider_delim(cols = "name", delim = ",", names = c("param", "time", "region")) %>%
+  mutate(param = as.character(gsub("phi\\[", "", param)),
+         time = as.numeric(time),
+         region = as.numeric(gsub("\\]", "", region)),
+         param = case_when(param == "1" ~ "lambda",
+                           param == "2" ~ "kappa",
+                           TRUE ~ param)) %>%
+  pivot_wider(names_from = param, values_from = value)
+
 pi_prob <- `sigma_ri_theta-ri_gamma-ri`$pi_prob %>% as_draws_df() %>%
   select(-c(".iteration", ".chain")) %>% 
   pivot_longer(cols = !".draw") %>%
@@ -92,6 +118,41 @@ pi_prob <- `sigma_ri_theta-ri_gamma-ri`$pi_prob %>% as_draws_df() %>%
          region = as.numeric(gsub("\\]", "", region)),
          name = "pi") %>% 
   pivot_wider(names_from = name, values_from = value)
+
+gamma <- `sigma_ri_theta-ri_gamma-ri`$gamma %>% as_draws_df() %>%
+  select(-c(".iteration", ".chain")) %>% 
+  pivot_longer(cols = !".draw") %>%
+  rename(draw = ".draw") %>%
+  mutate(region = as.character(gsub("gamma\\[", "", name)),
+         region = as.numeric(gsub("\\]", "", region)),
+         name = "gamma") %>% 
+  pivot_wider(names_from = name, values_from = value)
+
+theta <- `sigma_ri_theta-ri_gamma-ri`$theta %>% as_draws_df() %>%
+  select(-c(".iteration", ".chain")) %>% 
+  pivot_longer(cols = !".draw") %>%
+  rename(draw = ".draw") %>%
+  mutate(region = as.character(gsub("theta\\[", "", name)),
+         region = as.numeric(gsub("\\]", "", region)),
+         name = "theta") %>% 
+  pivot_wider(names_from = name, values_from = value)
+
+phi_onereg <- phi %>% filter(region == 10) %>% group_by(time) %>% summarize(kappa = median(kappa),
+                                                                            medlambda = median(lambda)) %>% ungroup()
+p <- phi_onereg %>% rename(kappa = medkappa, lambda = medlambda) %>% pivot_longer(cols = !time, names_to = "param", values_to = "value") %>%
+  ggplot(aes(x = time, y = value, color = param)) + geom_line() + theme_classic()
+
+ggsave("full-model/figures/paper/phi_reg10_overtime.png", p, dpi = 320, bg = "white")
+
+
+saveRDS(kappa_vals, "full-model/figures/paper/mcmc_draws/gamma.RDS")
+saveRDS(gamma, "full-model/figures/paper/mcmc_draws/gamma.RDS")
+saveRDS(theta, "full-model/figures/paper/mcmc_draws/theta.RDS")
+saveRDS(lambda, "full-model/figures/paper/mcmc_draws/lambda.RDS")
+saveRDS(rand_int, "full-model/figures/paper/mcmc_draws/rand_int.RDS")
+saveRDS(phi, "full-model/figures/paper/mcmc_draws/phi.RDS")
+saveRDS(pi_prob, "full-model/figures/paper/mcmc_draws/pi_prob.RDS")
+
 
 all_params <- kappa_vals %>% 
   left_join(rand_int) %>% 
@@ -173,6 +234,34 @@ ri_map <- ri_map %>% group_by(param, region) %>% summarize(med_val = median(valu
 ri_map <- ri_map %>% ungroup()
 xi_map <- ri_map %>% filter(param == "xi") %>% select(-param) %>% mutate(med_val = exp(med_val))
 sigma_map <- ri_map %>% filter(param == "sigma") %>% select(-param) %>% mutate(med_val = exp(med_val))
+
+gamma_map <- gamma %>% group_by(region) %>% summarize(med_gamma = median(gamma)) %>% ungroup() %>% left_join(full_reg_key)
+eco_gamma <- ecoregions_geom %>% left_join(gamma_map)
+breaks <- classIntervals(c(min(eco_gamma$med_gamma) - .00001, eco_gamma$med_gamma), style = 'fixed', 
+                         fixedBreaks = c(-0.4, -0.2, 0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4), intervalClosure = 'left')
+eco_gamma <- eco_gamma %>% mutate(gamma_cat = cut(med_gamma, unique(breaks$brks)))
+p <- ecoregions_geom %>%
+  ggplot() +
+  geom_sf(size = .1, fill = 'white') +
+  geom_sf(data = eco_gamma,
+          aes(fill=gamma_cat), alpha = 0.6, lwd = 0, inherit.aes = FALSE) +
+  theme_void() + scale_fill_brewer(palette = "Spectral")
+ggsave("full-model/figures/paper/gamma_map.png", dpi = 320, bg ='white')
+
+theta_map <- theta %>% group_by(region) %>% summarize(med_theta = median(theta)) %>% ungroup() %>% left_join(full_reg_key)
+eco_theta <- ecoregions_geom %>% left_join(theta_map)
+breaks <- classIntervals(c(min(eco_theta$med_theta) - .00001, eco_theta$med_theta), style = 'fixed', 
+                         fixedBreaks = c(-3, -2, -1, 0, 1, 2), intervalClosure = 'left')
+eco_theta <- eco_theta %>% mutate(theta_cat = cut(med_theta, unique(breaks$brks)))
+p <- ecoregions_geom %>%
+  ggplot() +
+  geom_sf(size = .1, fill = 'white') +
+  geom_sf(data = eco_theta,
+          aes(fill=theta_cat), alpha = 0.6, lwd = 0, inherit.aes = FALSE) +
+  theme_void() + scale_fill_brewer(palette = "Spectral")
+ggsave("full-model/figures/paper/theta_map.png", dpi = 320, bg ='white')
+
+
 
 xi_vals_regions <- xi_map %>% left_join(full_reg_key)
 eco_xi <- ecoregions_geom %>% 

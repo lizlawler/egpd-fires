4516c8566c0da82edd37818145f2f74f529579c6
Liz Lawler <eslawler@gmail.com>
2023-08-28 16:40:11 -0600

    modify shell script to include 'theta-time_gamma-ri' combination

--- shell-scripts/run_joint_NUTS.sh
+++ shell-scripts/run_joint_NUTS.sh
@@ -8,9 +8,10 @@ conda activate stan
 
 stanc_exe="/projects/$USER/software/anaconda/envs/stan/bin/cmdstan/bin/stanc"
 modtype="joint"
-for modname in "kappa-ri" "sigma-ri"
+for modname in "all-reg" "kappa-ri" "sigma-ri"
 do
-for params in "theta-cst_gamma-cst" "theta-cst_gamma-ri" "theta-ri_gamma-cst" "theta-ri_gamma-ri"
+# for params in "theta-cst_gamma-cst" "theta-cst_gamma-ri" "theta-ri_gamma-cst" "theta-ri_gamma-ri"
+for params in "theta-time_gamma-ri"
 do
 # compile model and link c++ 
 inc_path="full-model/fire-sims/${modtype}/${modname}/"

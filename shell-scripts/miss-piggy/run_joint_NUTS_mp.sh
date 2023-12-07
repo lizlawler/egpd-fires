627196d2a36437f3a49fbe3fddbae1018c8f2ce3
Liz Lawler <eslawler@gmail.com>
2023-12-07 13:03:58 -0700

    fix file paths to run joint models on miss-piggy

--- shell-scripts/miss-piggy/run_joint_NUTS_mp.sh
+++ shell-scripts/miss-piggy/run_joint_NUTS_mp.sh
@@ -12,12 +12,12 @@ for params in "nu" "sigma"
 do
 # compile model and link c++ 
 inc_path="full-model/fire-sims/${modtype}/${modname}/stan/"
-object="full-model/fire-sims/${modtype}/${modname}/stan/${modname}_${params}"
+object="full-model/fire-sims/${modtype}/stan/${modtype}_${modname}_${params}"
 ${stanc_exe} ${object}.stan --include-paths=${inc_path}
 cmdstan_model ${object}
 sttime=$(date +"%d%b%Y_%H%M")
 export modtype modname params sttime
-nohup ./shell-scripts/miss-piggy/burn_joint_sampling_mp.sh > full-model/output/${modname}_${dataset}_${params}_${sttime}_mp.txt 2>&1 &
+nohup ./shell-scripts/miss-piggy/burn_joint_sampling_mp.sh > full-model/output/${modname}_${params}_${sttime}_mp.txt 2>&1 &
 sleep 1
 done
 done

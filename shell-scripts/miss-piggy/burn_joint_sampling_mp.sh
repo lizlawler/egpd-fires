b7120564932ccb0bd50ed8522ec7d96e59ee806c
Liz Lawler <eslawler@gmail.com>
2023-12-07 13:13:37 -0700

    create parallel chains through for-loop instead of 'num_chains' argument

--- shell-scripts/miss-piggy/burn_joint_sampling_mp.sh
+++ shell-scripts/miss-piggy/burn_joint_sampling_mp.sh
@@ -8,10 +8,10 @@ model="stan/${modtype}_${modname}_${params}"
 outbase="csv-fits/${modtype}_${modname}_${params}_${sttime}"
 
 # run model with 3 chains
-./${model} sample num_chains=3 num_warmup=1000 num_samples=1000 \
+for i in {1..5}
+  do
+    ./${model} sample num_warmup=1000 num_samples=1000 \
                   data file=${datafile} \
                   init=0.01 \
-                  output file=${outbase}.csv \
-                  num_threads=3
-
-echo "Model has finished running all 3 chains"
+                  output file=${outbase}_${i}.csv &
+  done

c9d10f9529ebc03d93bafbf8b178b29a1d75a9b4
Liz Lawler <eslawler@gmail.com>
2023-12-11 22:20:25 -0700

    change transformation on xi
    had left 'inv_logit' in one line, from when we were testing different links

--- shell-scripts/run_all_models.sh
+++ shell-scripts/run_all_models.sh
@@ -6,6 +6,10 @@
 
 shell-scripts/run_joint_NUTS.sh
 sleep 1
+shell-scripts/run_g1_NUTS.sh
+sleep 1
+shell-scripts/run_counts_NUTS.sh
+sleep 1
 shell-scripts/run_g2_NUTS.sh
 sleep 1
 shell-scripts/run_lognorm_NUTS.sh

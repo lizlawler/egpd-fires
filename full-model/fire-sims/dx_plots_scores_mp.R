71f2525dd8bfb9c9fa67099f09f2f9ff6e69efa0
Liz Lawler <eslawler@gmail.com>
2024-01-04 12:16:51 -0700

    Extract scores from joint models with G4 and G3 sub-models

--- full-model/fire-sims/dx_plots_scores_mp.R
+++ full-model/fire-sims/dx_plots_scores_mp.R
@@ -5,28 +5,31 @@
 # dataset <- args[4]
 # sttime <- args[5]
 
-type <- "burns"
-model <- "g4"
-params <- "sigma-ri_xi-ri_nu"
-dataset <- "erc"
+# type <- "burns"
+model <- "joint"
+params <- "joint_g3_nu"
+dataset <- "erc_fwi"
+date <- "\\d{2}Dec2023_\\d{4}"
 
 library(cmdstanr)
 check_cmdstan_toolchain(fix = TRUE, quiet = TRUE)
 library(MCMCvis)
 library(posterior)
+library(stringr)
 
-csvbase <- paste0("./full-model/fire-sims/", type, "/", model, "/csv-fits/")
-plotbase <- paste0("./full-model/figures/", type, "/", model, "/trace/")
-csvpattern <- paste0( model, "_", dataset, "_", params)
+csvbase <- paste0("./full-model/fire-sims/", model, "/csv-fits/")
+plotbase <- paste0("./full-model/figures/", model, "/trace/")
+csvpattern <- paste0(params, "_", date, "_", dataset)
 csvfiles <- paste0(csvbase, list.files(path = csvbase, pattern = csvpattern))
 
-csvfiles <- csvfiles[c(1,3)] # keeping chains that actually completed
+csvpattern <- str_remove(str_remove(basename(csvfiles[1]), "_\\d{1}.csv"), "_\\d{4}")
+# csvfiles <- csvfiles[c(1,3)] # keeping chains that actually completed
 
 print("Filenames being used are:")
 csvfiles
 
 print("Creating cmdstan fit object...")
-fit <- as_cmdstan_fit(csvfiles)
+fit <- as_cmdstan_fit(csvfiles[1:2])
 fit$diagnostic_summary()
 
 print("Creating mcmc list from cmdstan object...")
@@ -43,36 +46,42 @@ MCMCtrace(fitmcmc,
           filename = paste0(plotbase, csvpattern, "_rho.pdf"))
 print("rho traceplots created")
 
-print("Creating traceplot of betas")
-MCMCtrace(fitmcmc,
-          params = 'beta', 
-          ind = TRUE, 
-          open_pdf = FALSE, 
-          filename = paste0(plotbase, csvpattern, "_beta.pdf"))
-print("beta traceplots created")
+# print("Creating traceplot of betas")
+# MCMCtrace(fitmcmc,
+#           params = 'beta', 
+#           ind = TRUE, 
+#           open_pdf = FALSE, 
+#           filename = paste0(plotbase, csvpattern, "_beta.pdf"))
+# print("beta traceplots created")
+# 
+# print("Creating traceplot of phi...")
+# MCMCtrace(fitmcmc,
+#           params = 'phi', 
+#           ind = TRUE, 
+#           open_pdf = FALSE, 
+#           filename = paste0(plotbase, csvpattern, "_phi.pdf"))
+# print("phi traceplots created")
 
-print("Creating traceplot of phi...")
-MCMCtrace(fitmcmc,
-          params = 'phi', 
-          ind = TRUE, 
-          open_pdf = FALSE, 
-          filename = paste0(plotbase, csvpattern, "_phi.pdf"))
-print("phi traceplots created")
+# print("Extracting scores from model object...")
+# if (type == "burns") {
+#   train_loglik <- fit$draws(variables = "train_loglik")
+#   holdout_loglik <- fit$draws(variables = "holdout_loglik")
+#   train_twcrps <- fit$draws(variables = "train_twcrps")
+#   holdout_twcrps <- fit$draws(variables = "holdout_twcrps")
+#   scores <- list(train_loglik, holdout_loglik, train_twcrps, holdout_twcrps)
+#   names(scores) <- c("train_loglik", "holdout_loglik", "train_twcrps", "holdout_twcrps")
+# } else {
+#   train_loglik <- fit$draws(variables = "train_loglik")
+#   holdout_loglik <- fit$draws(variables = "holdout_loglik")
+#   scores <- list(train_loglik, holdout_loglik)
+#   names(scores) <- c("train_loglik", "holdout_loglik")  
+# }
 
 print("Extracting scores from model object...")
-if (type == "burns") {
-  train_loglik <- fit$draws(variables = "train_loglik")
-  holdout_loglik <- fit$draws(variables = "holdout_loglik")
-  train_twcrps <- fit$draws(variables = "train_twcrps")
-  holdout_twcrps <- fit$draws(variables = "holdout_twcrps")
-  scores <- list(train_loglik, holdout_loglik, train_twcrps, holdout_twcrps)
-  names(scores) <- c("train_loglik", "holdout_loglik", "train_twcrps", "holdout_twcrps")
-} else {
-  train_loglik <- fit$draws(variables = "train_loglik")
-  holdout_loglik <- fit$draws(variables = "holdout_loglik")
-  scores <- list(train_loglik, holdout_loglik)
-  names(scores) <- c("train_loglik", "holdout_loglik")  
-}
+holdout_loglik <- fit$draws(variables = "holdout_loglik_burn")
+holdout_twcrps <- fit$draws(variables = "holdout_twcrps")
+scores <- list(holdout_loglik, holdout_twcrps)
+names(scores) <- c("holdout_loglik_burn", "holdout_twcrps")
 
 filename <- paste0("full-model/fire-sims/model_comparison/extracted_values/", csvpattern, "_scores.RDS")
 saveRDS(scores, file = filename)

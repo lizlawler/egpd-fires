98284cc1d1f8b26f6a667ba1f4f2d78908dec53c
Liz Lawler <eslawler@gmail.com>
2024-06-12 21:23:45 -0600

    create shell scripts to run G2 and lognormal on MissPiggy

---
+++ shell_scripts/mp_server/run_lognorm_NUTS_mp.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# shell script to call sbatch
+#
+# cycle through loop and launch sbatch for every combination
+#
+trap '' HUP
+stanc_exe="/data/accounts/lawler/.conda/envs/stan/bin/cmdstan/bin/stanc"
+modtype="sizes"
+modname="lognorm"
+dataset="erc_fwi"
+for params in "sigma-ri" "sigma-cst"
+do
+# compile model and link c++ 
+inc_path="models/${modtype}/${modname}/stan/"
+object="models/${modtype}/${modname}/stan/${modname}_${params}"
+${stanc_exe} ${object}.stan --include-paths=${inc_path}
+cmdstan_model ${object}
+sttime=$(date +"%d%b%Y_%H%M")
+export modtype modname params dataset sttime
+nohup ./shell_scripts/mp_server/submodel_sampling_mp.sh > shell_scripts/console_output/${modname}_${params}_${dataset}_${sttime}_mp.txt 2>&1 &
+sleep 1
+done
